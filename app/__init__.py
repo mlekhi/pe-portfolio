9f99a93d06553b0cbb6b41797816bb7d24f37c9f
mlekhi <maya.lekhi1@gmail.com>
2023-06-26 13:14:38 -0400

    css styling updates

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,10 @@ def index():
 def maya():
     return render_template('index.html', title="Maya Lekhi", url=os.getenv("URL"), photo='maya', about_text='Rising Sophomore at Western University')
 
+@app.route("/joseph")
+def joseph():
+    return render_template('index.html', title="Joseph Ma", url=os.getenv("URL"), photo='logo', about_text='blank')
+
 @app.route("/dilnaz")
 def dilnaz():
     return render_template('index.html', title="Dilnaz Uasheva", url=os.getenv("URL"), photo='dilnaz', about_text='Rising Sophomore at Minerva University')
@@ -21,7 +25,24 @@ def dilnaz():
 @app.route("/hobbies")
 def hobbies():
     title = "Our Team's Hobbies"
-    hobbies_list = ["Cooking", "Reading", "Hiking", "Photography", "Gardening"]
+    hobbies_list = [
+        {
+            'title': 'Reading',
+            'image': 'reading.jpeg'
+        },
+        {
+            'title': 'Gardening',
+            'image': 'gardening.jpeg'
+        },
+        {
+            'title': 'Painting',
+            'image': 'painting.jpeg'
+        },
+        {
+            'title': 'Cooking',
+            'image': 'cooking.jpeg'
+        }
+    ]
 
     return render_template('hobbies.html', title=title, hobbies_list=hobbies_list)
 

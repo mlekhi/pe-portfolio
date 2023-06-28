61d286b4de241f405e3142b22ef667a6b73d1f94
gbhnjmk <duko102003@gmail.com>
2023-06-28 18:47:54 +0600

    added nav bar to hobbies.html created the main page with overall info (it was about Joseph's projects before) updated the individual portfolio pages added coordinates and info for Dilnaz's page created a function for processing coordinates updated text.py that was created by joseph

--- app/__init__.py
+++ app/__init__.py
@@ -1,71 +1,97 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-from app.text import about_text, work_text
+from app.text import about_text, work_text_joseph, work_text_dilnaz, about_text_dilnaz, work_text, about_text_maya, \
+    work_text_maya
 
 load_dotenv()
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    coords = [(34.037945, -117.677852), (45.231382, 16.577320), (45.537137, 119.137498)]
+def mapping(coords):
     markers = ""
 
     for id, (lat, lon) in enumerate(coords):
         # Create the marker and its pop-up for each shop
         idd = f"a{id}"
         markers += "var {idd} = L.marker([{latitude}, {longitude}]);\
-                    {idd}.addTo(map);".format(
+                            {idd}.addTo(map);".format(
             idd=idd,
             latitude=lat,
             longitude=lon,
         )
+    return coords, markers
 
+
+@app.route("/")
+def index():
+    coords = [(34.037945, -117.677852), (45.231382, 16.577320), (45.537137, 119.137498), (48.0196, 66.9237),
+              (61.5240, 105.3188), (41.3775, 64.5853), (38.9637, 35.2433), (37.0902, -95.7129)]
     # Render the page with the map
     return render_template(
         "index.html",
-        markers=markers,
-        lat=coords[0][0],
-        lon=coords[0][1],
+        markers=mapping(coords)[1],
+        lat=(mapping(coords))[0][0][0],
+        lon=(mapping(coords))[0][0][1],
         title="MLH Fellow",
         url=os.getenv("URL"),
         photo="logo",
         about_text=about_text,
-        work_text=work_text,
-    )
-
-
-@app.route("/maya")
-def maya():
-    return render_template(
-        "index.html",
-        title="Maya Lekhi",
-        url=os.getenv("URL"),
-        photo="maya",
-        about_text="Rising Sophomore at Western University",
+        work_text=work_text
     )
 
 
 @app.route("/joseph")
 def joseph():
+    coords = [(34.037945, -117.677852), (45.231382, 16.577320), (45.537137, 119.137498)]
+
+    # Render the page with the map
     return render_template(
         "index.html",
-        title="Joseph Ma",
+        markers=mapping(coords)[1],
+        lat=(mapping(coords))[0][0][0],
+        lon=(mapping(coords))[0][0][1],
+        title="Joseph",
         url=os.getenv("URL"),
         photo="logo",
-        about_text="blank",
+        about_text=about_text,
+        work_text=work_text_joseph,
+    )
+
+
+@app.route("/maya")
+def maya():
+    coords = [(34.037945, -117.677852), (45.231382, 16.577320), (45.537137, 119.137498)]
+
+    # Render the page with the map
+    return render_template(
+        "index.html",
+        markers=mapping(coords)[1],
+        lat=(mapping(coords))[0][0][0],
+        lon=(mapping(coords))[0][0][1],
+        title="Maya Lekhi",
+        url=os.getenv("URL"),
+        photo="profile",
+        about_text=about_text_maya,
+        work_text=work_text_maya,
     )
 
 
 @app.route("/dilnaz")
 def dilnaz():
+    coords = [(48.0196, 66.9237), (61.5240, 105.3188), (41.3775, 64.5853), (38.9637, 35.2433), (37.0902, -95.7129)]
+
+    # Render the page with the map
     return render_template(
         "index.html",
+        markers=mapping(coords)[1],
+        lat=(mapping(coords))[0][0][0],
+        lon=(mapping(coords))[0][0][1],
         title="Dilnaz Uasheva",
         url=os.getenv("URL"),
         photo="dilnaz",
-        about_text="Rising Sophomore at Minerva University",
+        about_text=about_text_dilnaz,
+        work_text=work_text_dilnaz,
     )
 
 
--- app/text.py
+++ app/text.py
@@ -20,7 +20,7 @@ about_text = [
     "Overall, the MLH Fellowship provides an immersive and comprehensive experience that empowers aspiring technologists to develop their skills, work on real-world projects, and accelerate their careers in the tech industry. It serves as a launching pad for the next generation of innovators and problem solvers.",
 ]
 
-work_text = [
+work_text_joseph = [
     "",
     "1. Project: Sentiment Analysis for Social Media Monitoring",
     "",
@@ -41,4 +41,35 @@ work_text = [
     "5. Project: Personalized Recommendation Engine",
     "",
     "Description: Played a key role in developing a personalized recommendation engine for MLH's e-commerce platform. Leveraged collaborative filtering and content-based filtering techniques to analyze user behavior and preferences. Integrated the recommendation engine with MLH's existing product catalog, enabling personalized product recommendations to enhance user experience and increase customer engagement.",
+]
+
+work_text_dilnaz = [
+    "",
+    "1. Project: Aziz ",
+    "",
+    "Description: Build a compact sensor device via Arduino to aid rescue teams in their search for disaster victims that integrates various metrics to assess signs of human life from beneath rubble, allowing rescue workers to find victims trapped underground. Integrated IR, microwave, and ultrasound sensors to increase the accuracy of the output. Engineered a 3D model of a spherical skeleton via Autodesk Maya with adjustable edges to increase the motion in the rubble.",
+    "",
+    "2. Project: PulseBud",
+    "",
+    "Description: Programmed a real-time seizure prediction system for smartwatches powered by machine learning that provides an emergency notification system and instant access to critical medical records for healthcare workers.  Received a $3,000 monetary grant from InterSystems and Zepp Health Corp sponsorship.  Won I Place on CalHacks Hackathon by UC Berkeley.",
+    "",
+    "3. Project: AstroTravel",
+    "",
+    "Description: Developed a mobile app with a 3D model of the Solar System that predicts the planetary positions over time on Unity Engine & Vuforia for AR integration. Programmed rocket's flight trajectory prediction on the PyQt5 using Matplotlib to model efficient combination of motors, form, and launch positions through GFS (Global Forecast System).",
+    ""
+]
+
+about_text_dilnaz = [
+    "Rising Sophomore at Minerva university. Result-oriented software developer with an experience in mobile game dev on Unity, Python Industrial programming through Yandex Lyceum courses, and electrical engineering through personal projects and competitions. I seek to work as a software engineer to further my knowledge, implement innovative solutions and manage complex tasks."
+]
+
+work_text = [
+    "We are great team!"
+]
+
+work_text_maya = [
+
+]
+about_text_maya = [
+
 ]

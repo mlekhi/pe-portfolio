1d0516c3433482836e92a99956fc7ac19690a335
Joseph Maa <josephgmaa@berkeley.edu>
2023-06-27 23:39:27 -0700

    Update formatting for text, proof of concept

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from app.text import about_text, work_text
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,43 +9,61 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), photo='logo', about_text="I'm an MLH Fellow!", work_text="I've worked at x, y, z.")
+    return render_template(
+        "index.html",
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        photo="logo",
+        about_text=about_text,
+        work_text=work_text,
+    )
+
 
 @app.route("/maya")
 def maya():
-    return render_template('index.html', title="Maya Lekhi", url=os.getenv("URL"), photo='maya', about_text='Rising Sophomore at Western University')
+    return render_template(
+        "index.html",
+        title="Maya Lekhi",
+        url=os.getenv("URL"),
+        photo="maya",
+        about_text="Rising Sophomore at Western University",
+    )
+
 
 @app.route("/joseph")
 def joseph():
-    return render_template('index.html', title="Joseph Ma", url=os.getenv("URL"), photo='logo', about_text='blank')
+    return render_template(
+        "index.html",
+        title="Joseph Ma",
+        url=os.getenv("URL"),
+        photo="logo",
+        about_text="blank",
+    )
+
 
 @app.route("/dilnaz")
 def dilnaz():
-    return render_template('index.html', title="Dilnaz Uasheva", url=os.getenv("URL"), photo='dilnaz', about_text='Rising Sophomore at Minerva University')
+    return render_template(
+        "index.html",
+        title="Dilnaz Uasheva",
+        url=os.getenv("URL"),
+        photo="dilnaz",
+        about_text="Rising Sophomore at Minerva University",
+    )
+
 
 @app.route("/hobbies")
 def hobbies():
     title = "Our Team's Hobbies"
     hobbies_list = [
-        {
-            'title': 'Reading',
-            'image': 'reading.jpeg'
-        },
-        {
-            'title': 'Gardening',
-            'image': 'gardening.jpeg'
-        },
-        {
-            'title': 'Painting',
-            'image': 'painting.jpeg'
-        },
-        {
-            'title': 'Cooking',
-            'image': 'cooking.jpeg'
-        }
+        {"title": "Reading", "image": "reading.jpeg"},
+        {"title": "Gardening", "image": "gardening.jpeg"},
+        {"title": "Painting", "image": "painting.jpeg"},
+        {"title": "Cooking", "image": "cooking.jpeg"},
     ]
 
-    return render_template('hobbies.html', title=title, hobbies_list=hobbies_list)
+    return render_template("hobbies.html", title=title, hobbies_list=hobbies_list)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()

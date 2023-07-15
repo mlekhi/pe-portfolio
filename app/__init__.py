ec154a9537bd3d58ff6ade969303ec380a2aa074
gbhnjmk <duko102003@gmail.com>
2023-07-15 11:30:10 -0700

    Added the timeline.html and created a route to submit posts & output the previous ones

--- app/__init__.py
+++ app/__init__.py
@@ -168,5 +168,17 @@ def delete_timeline_post():
     else:
         abort(404)
 
+
+@app.route('/timeline', methods=["GET", "POST"])
+def timeline():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        content = request.form['content']
+        timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    timeline_posts = TimelinePost.select().order_by(TimelinePost.created_at.desc())
+    return render_template('timeline.html', title="Timeline", timeline_posts=timeline_posts)
+
 if __name__ == "__main__":
     app.run()

1d0091c8cc0c7f327b11fb9b5f0e2f6088efba5c
gbhnjmk <duko102003@gmail.com>
2023-07-15 14:25:03 -0700

    Changed the deployment file using systemctl

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-session -a
-tmux kill-session -t pe-portfolio
 
 cd pe-portfolio
 
@@ -10,7 +8,5 @@ git fetch && git reset origin/main --hard
 source ./python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s pe-portfolio
-tmux send-keys -t pe-portfolio 'cd pe-portfolio' Enter
-tmux send-keys -t pe-portfolio 'source /python3-virtualenv/bin/activate' Enter
-tmux send-keys -t pe-portfolio 'flask run --host=24.199.97.117' Enter
+systemctl daemon-relaod
+systemctl restart myportfolio

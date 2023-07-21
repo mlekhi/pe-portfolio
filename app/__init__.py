189d12c97df9fb2cce70422ac37a4b822de93dd8
mlekhi <maya.lekhi1@gmail.com>
2023-07-21 15:01:11 -0400

    adding env files

--- app/__init__.py
+++ app/__init__.py
@@ -45,44 +45,6 @@ def mapping(coords):
 
 @app.route("/")
 def index():
-    coords = [(34.037945, -117.677852), (45.231382, 16.577320), (45.537137, 119.137498), (48.0196, 66.9237),
-              (61.5240, 105.3188), (41.3775, 64.5853), (38.9637, 35.2433), (37.0902, -95.7129)]
-    # Render the page with the map
-    return render_template(
-        "index.html",
-        markers=mapping(coords)[1],
-        lat=(mapping(coords))[0][0][0],
-        lon=(mapping(coords))[0][0][1],
-        title="MLH Fellow",
-        url=os.getenv("URL"),
-        photo="logo",
-        about_text=about_text,
-        work_text=work_text,
-        education_text=education_text
-    )
-
-
-@app.route("/joseph")
-def joseph():
-    coords = [(34.037945, -117.677852), (45.231382, 16.577320), (45.537137, 119.137498)]
-
-    # Render the page with the map
-    return render_template(
-        "index.html",
-        markers=mapping(coords)[1],
-        lat=(mapping(coords))[0][0][0],
-        lon=(mapping(coords))[0][0][1],
-        title="Joseph",
-        url=os.getenv("URL"),
-        photo="logo",
-        about_text=about_text,
-        work_text=work_text_joseph,
-        education_text=education_text_joseph
-    )
-
-
-@app.route("/maya")
-def maya():
     coords = [(19.43260, -99.133209), (39.9526, -75.1652), (6.3690, 34.8888), (52.3676,4.9041)]
 
     # Render the page with the map
@@ -98,26 +60,6 @@ def maya():
         work_text=work_text_maya,
         education_text=education_text_maya
     )
-
-
-@app.route("/dilnaz")
-def dilnaz():
-    coords = [(48.0196, 66.9237), (61.5240, 105.3188), (41.3775, 64.5853), (38.9637, 35.2433), (37.0902, -95.7129)]
-
-    # Render the page with the map
-    return render_template(
-        "index.html",
-        markers=mapping(coords)[1],
-        lat=(mapping(coords))[0][0][0],
-        lon=(mapping(coords))[0][0][1],
-        title="Dilnaz Uasheva",
-        url=os.getenv("URL"),
-        photo="dilnaz",
-        about_text=about_text_dilnaz,
-        work_text=work_text_dilnaz,
-        education_text=education_text_dilnaz
-    )
-
 
 @app.route("/hobbies")
 def hobbies():
---
+++ curl-test.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+response=$(curl -X POST http://localhost:5000/api/timeline_post -d 'name=Maya&email=maya.lekhi1@gmail.com&content=testing endpoints with postman and curl.')
+echo "POST response: $response"
+
+response2=$(curl http://localhost:5000/api/timeline_post)
+echo "GET response: $response2"

62810f78a5baa2fd2d5fed171dceababb5f13c00
mlekhi <maya.lekhi1@gmail.com>
2023-07-26 16:23:48 -0400

    adding unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,16 +16,33 @@ class AppTestCase(unittest.TestCase):
                 html = response.get_data(as_text=True)
                 assert "<title>Maya Lekhi</title>" in html
                 # TODO Add more tests relating to the home page
+                assert "<img" in html
+                
         
         def test_timeline(self):
                 response = self.client.get("/api/timeline_post")
                 assert response.status_code == 200
                 assert response.is_json
                 json = response.get_json()
-                
-                print(json)  # Add this debugging statement
+
                 assert "timeline_post" in json
                 assert len(json["timeline_post"]) == 0
+  
+        # TODO Add more tests relating to the /api/timeline_post GET and POST apis 
+        def test_get(self):
+                response = self.client.get("/api/timeline_post")
+                assert response.status_code == 200
 
-                # TODO Add more tests relating to the /api/timeline_post GET and POST apis 
-                # TODO Add more tests relating to the timeline page
+        def test_post(self):
+                 data = {
+                        'name': 'John Doe',
+                        'email': 'john@example.com',
+                        'content': "Hello world, I'm John!"}
+                response = self.client.post("/api/timeline_post", json=data)
+                assert response.status_code == 201
+
+        # TODO Add more tests relating to the timeline page
+        def test_timeline_access(self):
+                response = self.client.get("/timeline")
+                assert response.status_code == 200
+

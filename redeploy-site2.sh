ec24a640145cb721d65d4f3ec030e590e94bc20e
mlekhi <maya.lekhi1@gmail.com>
2023-07-27 23:11:09 -0400

    adding script files

---
+++ redeploy-site2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# change directory to project folder
+cd pe-portfolio
+
+# fetch latest changes from main branch
+git fetch
+
+# reset the local repository to the latest changes from the main branch
+git reset origin/main --hard
+
+# activate Python venv and install dependencies
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+# restart portfolio service
+sudo systemctl restart myportfolio

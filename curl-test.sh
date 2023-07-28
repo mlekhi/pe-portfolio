d7ec3fdb169464618a648fc983132118855de701
Maya <121539073+mlekhi@users.noreply.github.com>
2023-07-27 23:08:55 -0400

    Update curl-test.sh

--- curl-test.sh
+++ curl-test.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 response=$(curl -X POST http://localhost:5000/api/timeline_post -d 'name=Maya&email=maya.lekhi1@gmail.com&content=testing endpoints with postman and curl.')
 echo "POST response: $response"

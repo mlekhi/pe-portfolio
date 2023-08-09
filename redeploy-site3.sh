47a951964b973dd73c93e16ec8cb53549cbafb86
mlekhi <maya.lekhi1@gmail.com>
2023-08-09 14:14:45 -0400

    changing redeploy

--- redeploy-site3.sh
+++ redeploy-site3.sh
@@ -6,10 +6,10 @@ cd pe-portfolio
 # fetch latest changes from main branch
 git fetch && git reset origin/main --hard
 
+# activate python virtual environment
+source python3-virtualenv/bin/activate
+
 # rerun docker
 docker compose -f docker-compose.prod.yml down
-
-# Remove the mysql container specifically
-docker rm mysql
 
 docker compose -f docker-compose.prod.yml up -d --build

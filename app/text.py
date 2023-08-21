92ed4143ca45fe62490791eb25279bcbb23f1c84
mlekhi <maya.lekhi1@gmail.com>
2023-08-21 11:53:06 -0400

    css styling for work section

--- app/text.py
+++ app/text.py
@@ -29,15 +29,10 @@ education_text = [
 ]
 
 work_text_maya = [
-    "",
     "Technology Operations Intern @ TD Bank",
-    "",
     "Leveraged Confiform, JSON, HTML to create an automated Confluence & Jira hub to facilitate quantifying the impact of TD’s Agile @Scale practice, streamlining internal operations for 700 practitioners.",
-    "",
     "Front-End UI/UX Developer @ NextGen EduCare",
-    "",
     "Crafted intuitive and user-friendly mobile app interfaces using HTML and CSS incorporating emerging design trends from social media platforms. Wireframed ~20 frames for app prototype to be presented to potential investors.",
-    ""
 ]
 about_text_maya = [
     "Sophomore at Western University studying Business Administration and Computer Science. Innovative software developer with a passion for creation and experience in fullstack & mobile development."
